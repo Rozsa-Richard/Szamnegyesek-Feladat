f26b94f73f1efac6fdeb079a5a539650d3588cd2
Rozsa-Richard <rozsarichardzon@gmail.com>
2026-01-26 20:05:44 +0100

    React befejezve

--- FourthTaskInReact/src/App.tsx
+++ FourthTaskInReact/src/App.tsx
@@ -7,21 +7,40 @@ import { toast } from 'react-toastify';
 
 function App() {
   const [fours, setFours] = useState<Array<Four>>();
+  const [input, setInput] = useState<String>();
+  const [id, setId] = useState<number>();
 
-  useEffect(()=> {
+  const getFours = () => {
     apiClient.get("/fours").then(r => setFours(r.data)).catch(()=> toast.error("Négyesek betöltése sikertelen"));
-  },[]);
+  }
+
+  useEffect(getFours,[]);
 
   return (
     <>
-      <h1>Számnégyesek</h1>
+      <h1 className='mb-5'>Számnégyesek</h1>
       <Row>
         <Col>
           <Card>
             <Card.Body>
               <h4>Hozzáadás</h4>
-              <input type="text" className='mt-3'/>
-              <Button variant='success' className='mt-3'>Hozzáadás</Button>
+              <input type="text" className='mt-3' onChange={e => setInput(e.target.value)}/> <br />
+              A négyest vesszővel elválasztva szóköz nélkül add meg! <br />
+              <Button variant='success' className='mt-3' onClick={() => {
+                apiClient.post("/fours", {four : "["+input+"]"}).then(() => toast.success("Sikeres hozzáadás")).catch(e => toast.error(e.response.data.message));
+                getFours();
+              }}>Hozzáadás</Button>
+            </Card.Body>
+          </Card>
+
+          <Card className='mt-5'>
+            <Card.Body>
+              <h4>Keresés</h4>
+              <input type="text" className='mt-3' onChange={e => setId(Number(e.target.value))}/> <br />
+              Add meg a megkeresni kívánt azonosítójú négyes! <br />
+              <Button variant='success' className='mt-3' onClick={() => {
+                apiClient.get(`/fours/${id}`).then((r) => toast.success("Találat: "+r.data.fours)).catch(e => toast.error(e.response.data.message));
+              }}>Hozzáadás</Button>
             </Card.Body>
           </Card>
         </Col>
